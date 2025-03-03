029d8dce8b7f8785e5dff8e530c93ef32ba9441c
GreedyOrc <ray.banks7@gmail.com>
2025-03-03 19:15:56 +0000

    Working navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { AboutMainComponent } from './about/components/about-main/about-main.component';
 import { MapBaseComponent } from './map/components/map-base/map-base.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
+import { DataMainComponent } from './data/components/data-main/data-main.component';
 
 export const routes: Routes = [
     
     {path: 'about', component: AboutMainComponent}, 
-    {path: 'map', component: MapBaseComponent}
-
+    {path: 'map', component: MapBaseComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component:RegisterComponent},
+    {path: 'data', component:DataMainComponent},
+    {path: '', component: MapBaseComponent}
 
 ];
---
+++ src/app/data/components/data-main/data-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-main',
+  imports: [],
+  templateUrl: './data-main.component.html',
+  styleUrl: './data-main.component.css'
+})
+export class DataMainComponent {
+
+}
